cd52d8f70027f4c6dde278d7eaf1b4c377058fa3
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-10-13 13:38:49 +0530

    completed

--- controllers/github.controller.js
+++ controllers/github.controller.js
@@ -1,5 +1,7 @@
 
 const githubModel = require("../model/github");
+
+//For searching github user
 const getAllUsers = async (req, res) => {
   try {
     const githubuser = await githubModel.find({ name: req.params.name });
@@ -10,4 +12,83 @@ const getAllUsers = async (req, res) => {
   }
 };
 
-module.exports = { getAllUsers };
+//For searching github user location
+const getUserLocation = async (req, res) => {
+    try {
+      const githubuserlocation = await githubModel
+        .find({ locatioin: req.params.location })
+        
+      return res.status(200).send(githubuserlocation);
+    } catch (err) {
+      return res.status(500).send({ message: err.message });
+    }
+};
+
+//For soft deleting a user
+const githubuserdelete = async (req, res) => {
+     try {
+       const { id } = req.params;
+       const githubuser = await githubModel.updateOne(
+         { _id: id },
+         { $set: { deleted: true } }
+       );
+
+       return res.status(200).send(JSON.stringify(githubuser));
+     } catch (err) {
+       return res.status(500).send({ message: err.message });
+     }
+}
+//For updating a github user
+const updateUser = async (req, res) => {
+  try {
+    const githube = await githubModel
+      .findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      })
+      .lean()
+      .exec();
+
+    return res.status(200).send(githube);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+//For sorting
+const sortUser = async (req, res, next) => { 
+    try {
+      const githube = await githubModel.find().sort({ size: 1 });
+
+      return res.status(200).send(githube);
+    } catch (err) {
+      return res.status(500).send({ message: err.message });
+    }
+}
+const sortCreated = async (req, res, next) => { 
+    try {
+      const githube = await githubModel.find().sort({ created_at: 1 });
+
+      return res.status(200).send(githube);
+    } catch (err) {
+      return res.status(500).send({ message: err.message });
+    }
+};
+const sortUpdate = async (req, res, next) => {
+    try {
+      const githube = await githubModel.find().sort({ updated_at: 1 });
+
+      return res.status(200).send(githube);
+    } catch (err) {
+      return res.status(500).send({ message: err.message });
+    }
+};
+
+module.exports = {
+  getAllUsers,
+  getUserLocation,
+  githubuserdelete,
+  updateUser,
+  sortUser,
+  sortCreated,
+  sortUpdate,
+};
--- routes/github.js
+++ routes/github.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const githubtRouter = express.Router();
-const { getAllUsers } = require("../controllers/github.controller");
+const {
+  getAllUsers,
+  getUserLocation,
+  githubuserdelete,
+  updateUser,
+  sortUser,
+  sortCreated,
+  sortUpdate,
+} = require("../controllers/github.controller");
 
 
 githubtRouter.get("/username/:name", getAllUsers);
+githubtRouter.get("/location/:location",getUserLocation);
+githubtRouter.delete("delete/:id", githubuserdelete);
+githubtRouter.patch("/update/:id", updateUser);
+githubtRouter.get("/sort/size", sortUser);
+githubtRouter.get("/sort/created", sortCreated);
+githubtRouter.get("/sort/updated", sortUpdate);
 
 module.exports = githubtRouter;
